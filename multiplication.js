f3746aea9297e131553a4d0c348f7aee5c3e10f6
sauertova <148710594+sauertova@users.noreply.github.com>
2023-12-17 13:49:56 -0500

    Add files via upload

---
+++ multiplication.js
@@ -0,0 +1,15 @@
+function multiplyArrays(arr1, arr2){
+
+  var result = [];
+
+  for (var i = 0; i < arr1.length; i++)  {
+}
+
+return result;
+}
+
+var array1 = [5, 10, 15];
+var array2 = [20, 25, 30]
+var multipliedArray = multiplyArrays(array1, array2);
+
+console.log(multipliedArray);
